1ea907be5e1999e9619040231f36be4ef27b0e13
Adam Parkin <aparkin1@SOCNET.SOC.PLYMOUTH.AC.UK>
2019-11-04 15:30:58 +0000

    e

---
+++ WebApplication_test/WebApplication_test/Models/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication_test.Models
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
